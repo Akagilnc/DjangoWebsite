b42aec9720b7d99cff33a5844add886387a34c5f
DeronLee <akagilnc@gmail.com>
2016-08-14 14:53:22 +0800

    can work with out style

--- music/views.py
+++ music/views.py
@@ -1,15 +1,30 @@
-from django.http import HttpResponse
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '%d/' % album.id
-        html += '<a href="%s">%s</a><br>' % (url,album.album_title)
-    return HttpResponse(html)
+    context = {'all_albums': all_albums}
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: %s </h2" % str(album_id))
+    # album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
+    context = {'album': album}
+    return render(request, 'music/detail.html', context)
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
